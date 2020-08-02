cea957de64ee8930caf448d7ae9d3a226466fd71
BobGore <44136626+BobGore@users.noreply.github.com>
2020-08-02 23:48:32 +0100

    Update cachematrix.R
    Initial attempt at solution to R Programming Assignment 2: Lexical Scoping--caching of the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2: Lexical Scoping--caching of the inverse of a matrix
+## The function makeCacheMatrix creates a special "matrix"
+## This is a list containing a function to
 
-## Write a short comment describing this function
+## 1. set the value of the matrix  
+## 2. get the value of the matrix 
+## 3. set the value of the inverse of the matrix - setinverse
+## 4. get the value of the inverse of the matrix - getinverse
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+            x <<- y
+            inv <<- NULL
 
-}
+    }
+    get <- function()x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get, 
+         setInverse = setInverse, 
+         getInverse = getInverse)
 
-
-## Write a short comment describing this function
+}  
+## The function cacheSolve calculates the value of the inverse of the a special "matrix"
+## from the argument of the makeCacheMatrix function above
+## If first checks to see if the inverse has been calculated and if so, it 
+## retrieves the inverse fron the cache
+## If the inverse has not been calculated the inverse is then calculated via the solve function
+## and the value of the inverse is set in the cache via the setInverse function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setInverse(inv)
+    inv
 }
